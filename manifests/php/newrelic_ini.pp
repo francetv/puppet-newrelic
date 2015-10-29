bd0a806415a006619fc29f6db603d2fde2a1eb96
Hans MOULRON <hans.moulron@francetv.fr>
2015-10-29 13:39:33 +0100

    php5enmod

--- manifests/php/newrelic_ini.pp
+++ manifests/php/newrelic_ini.pp
@@ -4,13 +4,14 @@ define newrelic::php::newrelic_ini (
   $exec_path,
 ) {
 
-  exec { "/usr/bin/newrelic-install ${name}":
+  exec { "/usr/bin/newrelic-install ${name}" :
     path     => $exec_path,
     command  => "/usr/bin/newrelic-install purge ; NR_INSTALL_SILENT=yes NR_INSTALL_KEY=${newrelic_license_key} /usr/bin/newrelic-install install",
     provider => 'shell',
     user     => 'root',
     group    => 'root',
     unless   => "grep ${newrelic_license_key} ${name}/newrelic.ini",
+    notify  => Exec["remove_old_ini_file"],
   }
 
   exec { "remove_old_ini_file" :
@@ -20,13 +21,22 @@ define newrelic::php::newrelic_ini (
     user     => 'root',
     group    => 'root',
     unless   => "grep ${newrelic_license_key} ${name}/newrelic.ini",
-    require  => Exec["/usr/bin/newrelic-install ${name}"],
   }
 
-  file { "${name}/newrelic.ini":
+  file { "${name}/newrelic.ini" :
     path    => "${name}/newrelic.ini",
     content => template('newrelic/newrelic.ini.erb'),
     require => Exec["/usr/bin/newrelic-install ${name}"],
+    notify  => Exec["php5enmod newrelic"]
+  }
+
+  exec { "php5enmod newrelic" :
+    path     => $exec_path,
+    command  => "php5enmod newrelic",
+    provider => 'shell',
+    user     => 'root',
+    group    => 'root',
+    unless   => "grep ${newrelic_license_key} ${name}/newrelic.ini",
   }
 
 }
