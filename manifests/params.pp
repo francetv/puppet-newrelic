8c55e312e6555c07ede3e3f6690fe9fb843a059f
Hans MOULRON <hans.moulron@francetv.fr>
2022-05-11 16:39:36 +0200

    add debian 11

--- manifests/params.pp
+++ manifests/params.pp
@@ -34,7 +34,7 @@ class newrelic::params {
       case $::operatingsystem {
         'Debian': {
           case $::operatingsystemrelease {
-            /^(10|9)/: {
+            /^(11|10|9)/: {
               if $::phpversion and $::phpversion =~ /^(?:(\d+)\.)?(?:(\d+)\.)?(\*|\d+)/ {
                 $majeur_version = $1
                 if $2 == undef {
